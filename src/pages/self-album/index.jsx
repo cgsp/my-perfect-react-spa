e69b0595ebb69149a0355a93a01add126adb14a1
guansaipeng <cgsp740178088@163.com>
2018-09-04 20:34:06 +0800

    下班发布一下

--- src/pages/self-album/index.jsx
+++ src/pages/self-album/index.jsx
@@ -2,7 +2,7 @@
  * @Author: John.Guan 
  * @Date: 2018-08-25 21:41:03 
  * @Last Modified by: John.Guan
- * @Last Modified time: 2018-09-04 19:52:19
+ * @Last Modified time: 2018-09-04 20:30:41
  */
 import React, { Component } from 'react'
 import { List, Form, Row, Col, Button, Input, DatePicker, message, Select, InputNumber } from 'antd'
@@ -392,26 +392,26 @@ class SelfTagTag extends Component {
     console.log('编辑', line)
     this.refs.mask.show()
     this.props.getCommonDimesionsAndTags(() => {
-      // apiAlbumGetMainPeople(line.sourceId)
-      //   .then(res => {
-      //     this.refs.mask.hide()
-      //     if (res.code !== ERR_OK) {
-      //       message.error(res.msg)
-      //       return
-      //     }
-      //     line.people = res.data
-      //     this.setState({
-      //       addOrEditTitle: '编辑自运营专辑',
-      //       addOrEditVisible: true,
-      //       addOrEditInitValues: line
-      //     })
-      //   })
-      this.setState({
-        addOrEditTitle: '编辑自运营专辑',
-        addOrEditVisible: true,
-        addOrEditInitValues: line
-      })
-      this.refs.mask.hide()
+      apiAlbumGetMainPeople(line.sourceId)
+        .then(res => {
+          this.refs.mask.hide()
+          if (res.code !== ERR_OK) {
+            message.error(res.msg)
+            return
+          }
+          line.people = res.data
+          this.setState({
+            addOrEditTitle: '编辑自运营专辑',
+            addOrEditVisible: true,
+            addOrEditInitValues: line
+          })
+        })
+      // this.setState({
+      //   addOrEditTitle: '编辑自运营专辑',
+      //   addOrEditVisible: true,
+      //   addOrEditInitValues: line
+      // })
+      // this.refs.mask.hide()
     })
   }
 
