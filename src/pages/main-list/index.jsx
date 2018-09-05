4eb6c767d1827db881590bf0083bcd9d5bfbb866
guansaipeng <cgsp740178088@163.com>
2018-09-05 11:10:49 +0800

    发布

--- src/pages/main-list/index.jsx
+++ src/pages/main-list/index.jsx
@@ -2,7 +2,7 @@
  * @Author: John.Guan 
  * @Date: 2018-08-25 21:41:03 
  * @Last Modified by: John.Guan
- * @Last Modified time: 2018-09-05 10:49:56
+ * @Last Modified time: 2018-09-05 11:08:12
  */
 import React, { Component } from 'react'
 import { List, Form, Row, Col, Button, Input, DatePicker, message, Select, InputNumber } from 'antd'
@@ -19,7 +19,7 @@ import TimeControlHoc from '@Components/time-control-hoc'
 import { connect } from 'react-redux'
 import { getCommonDimesions, getCommonDimesionsAndTags } from '@Redux/commonTagAndDimesion'
 
-import { apiSelfAlbumList, apiAlbumDetail, apiAlbumAddOrEdit, apiAlbumGetMainPeople } from '@Api/self-album'
+import { apiSelfAlbumList, apiAlbumDetail, apiAlbumAddOrEdit } from '@Api/self-album'
 
 import { commonSmallTypes } from '@Api'
 
