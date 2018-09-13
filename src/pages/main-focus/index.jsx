e08b42f967d56f83445e4cff597fba89e28806c6
guansaipeng <cgsp740178088@163.com>
2018-09-13 14:30:20 +0800

    发布了未知

--- src/pages/main-focus/index.jsx
+++ src/pages/main-focus/index.jsx
@@ -2,7 +2,7 @@
  * @Author: John.Guan 
  * @Date: 2018-08-25 21:41:03 
  * @Last Modified by: John.Guan
- * @Last Modified time: 2018-09-12 15:34:17
+ * @Last Modified time: 2018-09-13 14:28:10
  */
 import React, { Component } from 'react'
 import { List, Form, Row, Col, Button, Input, DatePicker, message, Select, InputNumber } from 'antd'
@@ -427,7 +427,6 @@ class MainFocus extends Component {
                 >
                   <Option value={1}>已上架</Option>
                   <Option value={2}>已下架</Option>
-                  <Option value={0}>未知</Option>
                 </Select>
               </FormItem>
             </Col>
