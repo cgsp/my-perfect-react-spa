a4b535bd3abee47ec1d3948adcf7bb616de01077
guansaipeng <cgsp740178088@163.com>
2018-09-25 17:58:41 +0800

    修改loading

--- src/components/mask-loading/index.jsx
+++ src/components/mask-loading/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { PropTypes } from 'prop-types'
 import './style'
-import loadingImg from './loading.gif'
+import loadingImg from './lodaing1.gif'
 
 export default class MaskLoading extends Component {
   static propTypes = {
@@ -9,7 +9,7 @@ export default class MaskLoading extends Component {
   }
 
   static defaultProps = {
-    msg: '拼命加载中...'
+    msg: ''
   }
 
   constructor() {
@@ -35,8 +35,12 @@ export default class MaskLoading extends Component {
     const content = (
       <div className="mask-loading">
         <div className="loading">
-          <img className="img" src={loadingImg} alt="" style={{ width: '50px', height: '50px' }} />
-          <p className="desc">{this.props.msg}</p>
+          <img className="img" src={loadingImg} alt="" style={{ width: '150px', height: 'auto' }} />
+          {
+            this.props.msg ?
+              <p className="desc">{this.props.msg}</p>
+              : null
+          }
         </div>
       </div>
 
--- src/pages/main-album/index.jsx
+++ src/pages/main-album/index.jsx
@@ -2,7 +2,7 @@
  * @Author: John.Guan 
  * @Date: 2018-08-25 21:41:03 
  * @Last Modified by: John.Guan
- * @Last Modified time: 2018-09-25 14:01:23
+ * @Last Modified time: 2018-09-25 17:55:40
  */
 import React, { Component } from 'react'
 import { List, Form, Row, Col, Button, Input, DatePicker, message, Select, InputNumber } from 'antd'
