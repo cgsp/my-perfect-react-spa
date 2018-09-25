4ff5cae1dbfb7cb21e4512f0edcf2633c3962899
guansaipeng <cgsp740178088@163.com>
2018-09-25 16:38:29 +0800

    全部好了

--- src/pages/child-table/edit/modules/tip-app/index.jsx
+++ src/pages/child-table/edit/modules/tip-app/index.jsx
@@ -12,17 +12,18 @@ class ModuleTipApp extends Component {
 
   render() {
     return (
-      <div className="tip-app-module">
+      <div className="tip-app-module-edit">
         <div className="module-title">
           <span className="text">提示下载App</span>
           <img className="delete" src={DeleteIcon} alt="delete" onClick={this.props.deleteModule} />
           <img className="move" src={MoveIcon} alt="move" />
         </div>
         <div className="module-content">
-          <img className="logo" src={Logo} alt="logo" />
-          <div className="text-box">
-            <div className="top">喜马拉雅FM-听书神器</div>
-            <div className="bottom">高品质，离线听，不耗费流量</div>
+          <div className="img-text">
+            <img className="logo" src={Logo} alt="logo" />
+            <div className="text-box">
+              高品质，离线听，不耗费流量
+          </div>
           </div>
           <button className="btn">下载App</button>
         </div>
