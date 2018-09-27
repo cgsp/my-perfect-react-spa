57ecae4382edc0a6f38314dd5f5486343ff628b6
guansaipeng <cgsp740178088@163.com>
2018-09-27 16:26:37 +0800

    全屏

--- src/dashboard/head-nav/index.jsx
+++ src/dashboard/head-nav/index.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
+import { Button, Icon } from 'antd'
 import './style.scss'
+import fullScreen from './full-screen.png'
+import fullScreenExit from './full-screen-exit.png'
 // import {Icon} from 'antd'
 // import Cookies from 'js-cookie'
 // import {withRouter, Link} from 'react-router-dom'
@@ -18,12 +21,65 @@ export default class HeadNav extends Component {
   //     console.log('退出')
   // }
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      full: false
+    }
+    this.fullScreen = this.fullScreen.bind(this)
+    this.fullScreenExit = this.fullScreenExit.bind(this)
+  }
+
+  fullScreen() {
+    var docElm = document.documentElement
+    if (docElm.requestFullscreen) {
+      docElm.requestFullscreen()
+    }
+    else if (docElm.msRequestFullscreen) {
+      docElm.msRequestFullscreen()
+    }
+    else if (docElm.mozRequestFullScreen) {
+      docElm.mozRequestFullScreen()
+    }
+    else if (docElm.webkitRequestFullScreen) {
+      docElm.webkitRequestFullScreen()
+    }
+    this.setState({
+      full: true
+    })
+  }
+
+  fullScreenExit() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen()
+    }
+    else if (document.msExitFullscreen) {
+      document.msExitFullscreen()
+    }
+    else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen()
+    }
+    else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen()
+    }
+    this.setState({
+      full: false
+    })
+  }
   render() {
     return (
       <div className="header-container">
         <div className="main-nav">
           <h1 style={{ fontSize: '20px' }}>开放平台自运营后台</h1>
-          <div className="user-panel" />
+          <div className="user-panel" >
+            <div className="fullscreen">
+              {
+                !this.state.full ?
+                  <img src={fullScreen} alt="" onClick={this.fullScreen} /> :
+                  <img src={fullScreenExit} alt="" onClick={this.fullScreenExit} />
+              }
+            </div>
+          </div>
         </div>
       </div>
     )
