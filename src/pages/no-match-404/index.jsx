b71658a2ff0e7495ad85c32300d231dd710dc0a7
guansaipeng <cgsp740178088@163.com>
2018-11-16 15:54:15 +0800

    提交

--- src/pages/no-match-404/index.jsx
+++ src/pages/no-match-404/index.jsx
@@ -5,7 +5,9 @@ class NoMatch404 extends Component {
 
   render() {
     return (
-      <div className="no-match-404">路径有误，或该用户不存在该页面的访问权限</div>
+      <div className="no-match-404">
+        路径有误，或该用户不存在该页面的访问权限
+      </div>
     )
   }
 }
