e97359134821aa7a12f4963637a8e76dee29707e
guansaipeng <cgsp740178088@163.com>
2018-11-21 15:48:20 +0800

    rem适配

--- src/utils/libraries/rem-resize.js
+++ src/utils/libraries/rem-resize.js
@@ -3,6 +3,7 @@
     resizeEvent = 'orientationchange' in window ? 'orientationchange' : 'resize',
     resetRootFontSize = function () {
       var clientWidth = docEl.clientWidth
+      console.log('clientWidth', clientWidth)
 
       if (!clientWidth) {
         return
@@ -13,6 +14,8 @@
       if (clientWidth >= 1080) {
         docEl.style.fontSize = '100px'
       } else {
+        // 设计稿，按照宽度750px来算
+        // 这样保证1rem，就是100px
         docEl.style.fontSize = 100 * (clientWidth / 750) + 'px'
       }
     }
