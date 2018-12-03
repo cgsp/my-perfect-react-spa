8923ab75357cbe4070d7d62f972e4318aa124017
guansaipeng <cgsp740178088@163.com>
2018-12-03 18:49:45 +0800

    测试

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 
 class Index extends Component {
   render() {
+    const arr = [[1, 2]].map(([a, b], index) => {
+      console.log(index)
+      return a + b
+    })
+    console.log(arr)
     return (
       <div>首页</div>
     )
