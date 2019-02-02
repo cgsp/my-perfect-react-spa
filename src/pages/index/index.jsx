b226bbda789aada3739349f072e6a0c635295aa6
guansaipeng <cgsp740178088@163.com>
2019-02-02 14:52:54 +0800

    使用forwardref

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,31 +1,24 @@
 import React, { Component } from 'react'
-import Test from '../test'
+// import Test from '../test'
 
-
-const FnComponent = () => {
+const FnComponent = React.forwardRef((props, ref) => {
   return (
-    <div>我是个无状态组件</div>
+    <input type="text" ref={ref} />
   )
-}
+})
 
 class Index extends Component {
   constructor(props) {
     super(props)
-    this.myInputRef1 = React.createRef()
-    this.myInputRef2 = React.createRef()
+    this.myref = React.createRef()
   }
   componentDidMount = () => {
-    this.myInputRef1.current.value = '1'
-    this.myInputRef2.current.value = '22222'
+    this.myref.current.value = '哈哈哈哈'
   }
 
   render() {
     return [
-      <div ref="strref" key="1">1</div>,
-      <input type="text" ref={this.myInputRef1} key="input1" />,
-      <input type="text" ref={this.myInputRef2} key="input2" />,
-      <Test ref="test" key="2" />,
-      <FnComponent key="3" />
+      <FnComponent key="1" ref={this.myref} />
     ]
   }
 }
